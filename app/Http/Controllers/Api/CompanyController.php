32749266f0e9b240f7f64a0c33a69100f5af09be
Renzo Siza Tejada <rsiza@unsa.edu.pe>
2024-04-24 20:03:08 -0500

    insercion de una compañia

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Company;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class CompanyController extends Controller
 {
@@ -13,7 +14,7 @@ class CompanyController extends Controller
      */
     public function index()
     {
-        return 'hola';
+        return JWTAuth::user();
     }
 
     /**
@@ -21,7 +22,38 @@ class CompanyController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'razon_social' => 'required|string|max:255',
+            'ruc' => 'required|string|max:255',
+            /* 'ruc' => [
+                'required',
+                'string',
+                'regex:/^(10|20)\d{9}$/',
+                new UniqueCompanyRule(),
+            ], */
+            'direccion' => 'required|string|max:255',
+            'logo' => 'nullable|file|image',
+            'sol_user' => 'required|string|max:255',
+            'sol_pass' => 'required|string|max:255',
+            'cert' => 'required|file|mimes:pem,txt',
+            'client_id' => 'nullable|string|max:255',
+            'client_secret' => 'nullable|string|max:255',
+            'production' => 'nullable|boolean',
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $data['logo_path'] = $request->file('logo')->store('logos');
+        }
+
+        $data['cert_path'] = $request->file('cert')->store('certs');
+        $data['user_id'] = auth()->id();
+
+        $company = Company::create($data);
+
+        return response()->json([
+            'message' => 'Empresa creada correctamente',
+            'company' => $company,
+        ], 201);
     }
 
     /**
